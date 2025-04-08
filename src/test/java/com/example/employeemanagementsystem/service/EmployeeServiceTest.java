ec8fd0c288f3f52a73579a4f332cc4d891c7fded
R-Kiselev <sputnavik@gmail.com>
2025-04-08 10:54:13 +0300

    feat: tests

---
+++ src/test/java/com/example/employeemanagementsystem/service/EmployeeServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.employeemanagementsystem.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EmployeeServiceTest {
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
